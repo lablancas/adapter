41601db9e6b4b7f5fa488eb30459914b3b708a84
lablancas <lablancas@gmail.com>
2015-04-08 06:55:03 +0000

    remove Travis CI logo because PhantomJS is not WebRTC compatible; add summary to package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'lablancas:webrtc-adapter',
     version: '0.0.1',
     // Brief, one-line summary of the package.
-    summary: '',
+    summary: 'Shim to insulate apps from spec changes and prefix differences',
     // URL to the Git repository containing the source code for this package.
     git: 'https://github.com/lablancas/adapter',
     // By default, Meteor will default to using README.md for documentation.
